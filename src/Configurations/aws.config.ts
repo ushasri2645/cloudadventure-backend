8956596bc0c8cc23415151b53306a883118f1693
ushasri2645 <ugudikandula@everest.engineering>
2025-03-16 11:04:58 +0530

    Configured AWS with region and access keys

---
+++ src/Configurations/aws.config.ts
@@ -0,0 +1,9 @@
+import AWS from "aws-sdk";
+
+export const AWSConfig = () => {
+  AWS.config.update({
+    region: "ap-south-1",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+};
