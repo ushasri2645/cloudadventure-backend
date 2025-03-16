c0a4adb4cc494cbd4af721672685ce673f3b6b80
ushasri2645 <ugudikandula@everest.engineering>
2025-03-16 11:30:33 +0530

    Update file with router import and usage line

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { router } from './src/routes/registerRouter';
+import { router } from './src/routes/register.router';
 
 export const app = express();
 app.use(express.json());
