620cbb2e89d41d780b2e39de51b8cd693d65a7b8
ushasri2645 <ugudikandula@everest.engineering>
2025-03-16 10:21:13 +0530

    Initialize application entry point

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import { router } from './src/routes/registerRouter';
+
+export const app = express();
+app.use(express.json());
+app.use(cors(
+  {
+    origin: 'https://d3dyzqhp8l13cs.cloudfront.net', 
+    methods: 'GET,POST,PUT,DELETE',   
+    credentials: true,   
+    allowedHeaders: ['Content-Type', 'Authorization']             
+  }
+))
+app.use('/',router);
+
+const PORT =  5050;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
