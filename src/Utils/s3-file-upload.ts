9886514533a38ffb6ee1876c0f7e3ca972fe14a5
ushasri2645 <ugudikandula@everest.engineering>
2025-03-16 13:41:13 +0530

    Add functionality to add images to s3 bucket

---
+++ src/Utils/s3-file-upload.ts
@@ -0,0 +1,30 @@
+import AWS from "aws-sdk";
+import fs from "fs";
+import dotenv from "dotenv";
+import { AWSConfig } from "../Configurations/aws.config";
+dotenv.config();
+
+export const s3FileUpload = (fileName: string, path: string) => {
+  try {
+    AWSConfig();
+    const s3 = new AWS.S3();
+    const params = {
+      Bucket: 'originalimagesbkt',
+      Key: fileName,
+      Body: fs.createReadStream(path),
+      ACL: "public-read",
+    };
+    s3.upload(params, (err: any, data: { Location: any }) => {
+      if (err) {
+        console.log("Error uploading file:", err);
+      } else {
+        console.log(
+          "File uploaded successfully. File location:",
+          data.Location
+        );
+      }
+    });
+  } catch (e:any) {
+    throw new Error(`Error occured while uploading file, ${e.message}`)
+  }
+};
