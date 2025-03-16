b11200bf7ba07ab5934b9874f8518244e319f1cc
ushasri2645 <ugudikandula@everest.engineering>
2025-03-16 22:44:17 +0530

    Update s3 upload using promise

--- src/Utils/s3-file-upload.ts
+++ src/Utils/s3-file-upload.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import { AWSConfig } from "../Configurations/aws.config";
 dotenv.config();
 
-export const s3FileUpload = (fileName: string, path: string) => {
+export const s3FileUpload = async(fileName: string, path: string) => {
   try {
     AWSConfig();
     const s3 = new AWS.S3();
@@ -14,16 +14,18 @@ export const s3FileUpload = (fileName: string, path: string) => {
       Body: fs.createReadStream(path),
       ACL: "public-read",
     };
-    s3.upload(params, (err: any, data: { Location: any }) => {
-      if (err) {
-        console.log("Error uploading file:", err);
-      } else {
-        console.log(
-          "File uploaded successfully. File location:",
-          data.Location
-        );
-      }
-    });
+    // s3.upload(params, (err: any, data: { Location: any }) => {
+    //   if (err) {
+    //     console.log("Error uploading file:", err);
+    //   } else {
+    //     console.log(
+    //       "File uploaded successfully. File location:",
+    //       data.Location
+    //     );
+    //   }
+    // });
+    const data = await s3.upload(params).promise();
+    console.log("image added succesfully"+data.Location)
   } catch (e:any) {
     throw new Error(`Error occured while uploading file, ${e.message}`)
   }
