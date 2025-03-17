c4e612497264fb81185788cdc36cc5fbaec3594a
ushasri2645 <ugudikandula@everest.engineering>
2025-03-17 10:46:56 +0530

    Update config

--- src/Configurations/aws.config.ts
+++ src/Configurations/aws.config.ts
@@ -1,7 +1,6 @@
 import AWS from "aws-sdk";
 
 export const AWSConfig = () => {
-  console.log(process.env.AWS_ACCESS_KEY_ID,process.env.AWS_SECRET_ACCESS_KEY)
   AWS.config.update({
     region: "ap-south-1",
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
